0ce0032355eafc0bd05aa278ffde771b5003c192
Italo Gabriel <108748417+Ytalul@users.noreply.github.com>
2024-05-13 12:55:46 +0000

    Marcelo alterou aqui

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world altered again");
+        System.out.println("Hello world From Marcelus");
 
     }
 }

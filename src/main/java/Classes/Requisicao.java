706edacc91434f3b234741b36f1e3e16ae15be2b
ytalo <ytalo4869@gmail.com>
2024-05-13 16:41:14 -0300

    Adicionando classe Requisicao

---
+++ src/main/java/Classes/Requisicao.java
@@ -0,0 +1,14 @@
+package Classes;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Requisicao {
+    public String enderecoIp;
+    public String identidadeCliente;
+    public int userID;
+    public LocalDateTime data;
+    public String tipoSolicitacao;
+    public String recursoSolicitado;
+    public int codResposta;
+    public int tamanhoObjeto;
+}
